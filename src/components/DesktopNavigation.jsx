89b1a451c3ffa5a0865ca9ee04e42eda2f4bc149
abiolasikal <sikal.sikal.ss@gmail.com>
2022-07-22 22:54:14 +0100

    Desktop and Mobile Navigation Components

---
+++ src/components/DesktopNavigation.jsx
@@ -0,0 +1,123 @@
+import {IoChevronDownSharp, IoGiftOutline, IoPeopleOutline, IoShapesOutline, IoTrophyOutline} from "react-icons/io5";
+import {MdOutlineMarkEmailUnread} from "react-icons/md";
+import {FiHeart} from "react-icons/fi";
+import {TbWorld} from "react-icons/tb";
+import {AiFillAndroid, AiOutlineApple} from "react-icons/ai";
+
+const DesktopNavigation = ({handleToggleBox, isFirstBoxClicked, isSecondBoxClicked, isThirdBoxClicked}) => {
+    return (
+        <div className={"hidden md:block space-x-12"}>
+            <div className={"flex items-center gap-x-12"}>
+                <button className={"text-[#002444] font-medium text-lg"}>Upskill</button>
+
+                <div className={"relative"}>
+                    <button
+                        onClick={() => handleToggleBox("first")}
+                        className={"text-[#002444] font-medium text-lg gap-2 flex items-center"}>
+                        Customers
+                        <span><IoChevronDownSharp className={"text-sm"}/></span>
+                    </button>
+
+                    {isFirstBoxClicked && <div
+                        className={"z-10 top-[45px] -left-[55px] bg-white absolute px-[30px] box w-[200px] py-[18px] space-y-7 rounded-2xl"}>
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-red-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <IoGiftOutline className={"text-red-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[14px] text-[#002444] font-semibold"}>User Rewards</p>
+                        </div>
+
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-green-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <IoTrophyOutline className={"text-green-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[14px] text-[#002444] font-semibold"}>Gift Card Rates</p>
+                        </div>
+
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-cyan-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <MdOutlineMarkEmailUnread className={"text-cyan-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[14px] text-[#002444] font-semibold"}>Contact Us</p>
+                        </div>
+                    </div>}
+                </div>
+
+                <div className={"relative"}>
+                    <button onClick={() => handleToggleBox("second")}
+                            className={"text-[#002444] font-medium text-lg gap-2 flex items-center"}>
+                        Business
+                        <span><IoChevronDownSharp className={"text-sm"}/></span>
+                    </button>
+
+                    {isSecondBoxClicked && <div
+                        className={"z-10 bg-white absolute top-[45px] -left-[55px] box w-[200px] px-[23px] py-[18px] space-y-7 rounded-2xl"}>
+                        <div className={"flex gap-3 items-center"}>
+                            <div
+                                className={"bg-red-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <IoPeopleOutline className={"text-red-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>Become A Partner</p>
+                        </div>
+
+                        <div className={"flex gap-3 items-center"}>
+                            <div
+                                className={"bg-green-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <FiHeart className={"text-green-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>#CTGivesBack</p>
+                        </div>
+
+                        <div className={"flex gap-3 items-center"}>
+                            <div
+                                className={"bg-cyan-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <IoShapesOutline className={"text-cyan-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>Work With Us</p>
+                        </div>
+                    </div>}
+                </div>
+
+                <div className={"relative"}>
+                    <button onClick={() => handleToggleBox("third")}
+                            className={"bg-[#FFEACA] text-[#002444] font-medium w-[160px] py-[15px] rounded-xl gap-2 flex items-center justify-center"}>
+                        Get Started
+                        <span><IoChevronDownSharp className={"text-sm"}/></span>
+                    </button>
+
+                    {isThirdBoxClicked && <div
+                        className={"z-10 top-[75px] bg-white absolute px-[30px] box w-[170px] py-[18px] space-y-7 rounded-2xl"}>
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-red-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <TbWorld className={"text-red-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>On the Web</p>
+                        </div>
+
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-green-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <AiFillAndroid className={"text-green-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>On Android</p>
+                        </div>
+
+                        <div className={"flex gap-2 items-center"}>
+                            <div
+                                className={"bg-cyan-300 bg-opacity-30 h-[25px] w-[25px] grid place-items-center rounded-full"}>
+                                <AiOutlineApple className={"text-cyan-600 text-lg"}/>
+                            </div>
+                            <p className={"text-[13px] text-[#002444] font-semibold"}>On iOS</p>
+                        </div>
+                    </div>}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DesktopNavigation;
---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import {Twirl as Hamburger} from "hamburger-react";
+import {AnimatePresence, motion} from "framer-motion";
+
+const MobileNavigation = ({isMobile, setIsMobile, boxVariant}) => {
+    const [isFirstMobileButtonClicked, setIsFirstMobileButtonClicked] = useState(false);
+    const [isSecondMobileButtonClicked, setIsSecondMobileButtonClicked] = useState(false);
+
+    return (
+        <div className={"md:hidden relative"}>
+            <Hamburger direction={"right"} toggled={isMobile} toggle={setIsMobile} size={20} easing={"ease-in"}/>
+
+            {/*the open mobile menu background with framer-motion */}
+            {isMobile ? <motion.div variants={boxVariant} animate={"open"} className={"bgContainer"}/> :
+                <motion.div variants={boxVariant} animate={"close"} className={"bgContainer"}/>}
+
+            <AnimatePresence>
+                {isMobile &&
+                    <motion.div transition={{delay: .7, type: "spring", stiffness: 100}} initial={{x: -200}}
+                                animate={{x: 0}} exit={{x: -200}}
+                                className={"fixed z-10 top-0 right-0 w-full h-full"}>
+                        <div className={"px-[60px] mt-[60%] h-fit flex flex-col gap-[62px]"}>
+                            <div className={"text-white font-medium text-lg"}>Upskill</div>
+                            <div className={"space-y-4"}>
+                                <div className={"text-white font-medium text-lg relative"}>
+                                    <button
+                                        onClick={() => setIsFirstMobileButtonClicked(!isFirstMobileButtonClicked)}>Customers
+                                    </button>
+                                </div>
+
+                                {isFirstMobileButtonClicked && <div
+                                    className={"bg-[#002444] flex flex-col border text-[14px] space-y-4 text-gray-400"}>
+                                    <p>User Rewards</p>
+                                    <p>Gift Card Rates</p>
+                                    <p>Contact Us</p>
+                                </div>}
+                            </div>
+                            <div className={"space-y-4"}>
+                                <div className={"text-white font-medium text-lg relative"}>
+                                    <button
+                                        onClick={() => setIsSecondMobileButtonClicked(!isSecondMobileButtonClicked)}>Business
+                                    </button>
+                                </div>
+
+                                {isSecondMobileButtonClicked && <div
+                                    className={"bg-[#002444] flex flex-col border text-[14px] space-y-4 text-gray-400"}>
+                                    <p>User Rewards</p>
+                                    <p>Gift Card Rates</p>
+                                    <p>Contact Us</p>
+                                </div>}
+                            </div>
+                            <div className={"space-y-4"}>
+                                <div className={"text-white font-medium text-lg relative"}>
+                                    <button
+                                        onClick={() => setIsSecondMobileButtonClicked(!isSecondMobileButtonClicked)}>
+                                        Get Started
+                                    </button>
+                                </div>
+
+                                {isSecondMobileButtonClicked && <div
+                                    className={"bg-[#002444] flex flex-col border text-[14px] space-y-4 text-gray-400"}>
+                                    <p>On the Web</p>
+                                    <p>On Android</p>
+                                    <p>On iOS</p>
+                                </div>}
+                            </div>
+                        </div>
+                    </motion.div>
+                }
+            </AnimatePresence>
+        </div>
+    );
+};
+
+export default MobileNavigation;
